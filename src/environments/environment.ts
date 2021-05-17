e37127e2227c19eda3158e095def9a0c38a68bd5
Mayara Felix <mayaralcabral@gmail.com>
2021-05-16 22:36:27 -0400

    Setup env variables.

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiBase: process.env.BASE_API_URL,
+  googleClientId: process.env.GOOGLE_CLIENT_ID
+};
